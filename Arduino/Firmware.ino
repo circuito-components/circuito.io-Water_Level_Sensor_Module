6863afc9e2a9d4c43633dd4d58181dbafe65a4b1
Ohad Rusnak <ohad@robo-plan.com>
2017-04-26 12:30:34 +0000

    firmware update

--- Arduino/Firmware.ino
+++ Arduino/Firmware.ino
@@ -1,11 +1,17 @@
-// Include Libraries
+// // ***************************** Include Libraries *************************************
 #include "Arduino.h"
+// ****************************************************************************************
 
 // Pin Definitions
+#define WATERLEVELSENSOR_PIN_SIG	A3
 
-// Global variables and defines
 
-// Constructors
+// ***************************** Global variables and defines *****************************
+
+// ****************************************************************************************
+// ******************************** object initialization *********************************
+
+// ****************************************************************************************
 
 /* This code sets up the essentials for your circuit to work. It runs first every time your circuit is powered with electricity. */
 void setup() {
@@ -15,12 +21,15 @@ void setup() {
     while (!Serial) ; // wait for serial port to connect. Needed for native USB
     Serial.println("start");
     
-    //WaterFlow - setup
-    
+    // ******************************* WaterLevelSensor - setup *****************************
+
+    // *************************************************************************************
 }
 
 /* This code is the main logic of your circuit. It defines the interaction between the components you selected. After setup, it runs over and over again, in an eternal loop. */
 void loop() {
-    //WaterFlow - Test Code
+    // ******************************* WaterLevelSensor - Test Code *************************
+   
+    // *************************************************************************************
     
 }
